8e3b39ee41fceca4a0c46ec0201afdf12803878b
Krishna Chaythanya <sailu@Krishnas-MacBook-Pro.local>
2024-10-11 11:13:05 -0400

    added list

---
+++ lists.py
@@ -0,0 +1,13 @@
+fruits=["apple","oranges","banana"]
+print(fruits)
+length=fruits.__len__()
+i=0
+while(i<length):
+    print(fruits[i])
+    i+=1
+fruits.pop()
+fruits.append("blueberry")
+print(fruits)
+fruits.sort()
+print(fruits)
+
